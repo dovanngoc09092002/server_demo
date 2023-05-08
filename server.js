b96ae44bbbc85895b35cb3fbfcb00f8178165263
Đỗ Văn Ngọc <88466979+dovanngoc09092002@users.noreply.github.com>
2023-05-08 11:07:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,6 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 
 //cookie parser
