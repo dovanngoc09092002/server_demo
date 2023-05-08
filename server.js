c62fa2e787583dc75002a0a64587f5172391fba8
Đỗ Văn Ngọc <88466979+dovanngoc09092002@users.noreply.github.com>
2023-05-08 10:43:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -39,14 +39,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 
 //server soketio = server
 const server = http.createServer(app);
